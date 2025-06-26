610a97bac2f804e33249a734bfc382bacf86d03f
AGhebrea <alex.ghebrea4@gmail.com>
2025-06-26 17:59:16 +0300

    debug enabled expression macro

--- src/ccdebug/include/ccdebug.h
+++ src/ccdebug/include/ccdebug.h
@@ -3,9 +3,28 @@
  * to use it you'd have to basically define the format string yourself
  * and that is the same as just calling ccLogTrace by hand */
 
+/* Can mess up the debug lineinfo if you
+ * don't use it like: 
+ * DebugEnabledExpression( 
+ *      line1;
+ *      line2;
+ *      etc...
+ * )
+ * 
+ * the release variant of the macro comments out
+ * expression just because of the same reason
+ * restriction is that you cannot have
+ * comments inside of the DebugEnabledExpression
+ * macro
+ */
 #ifdef DEBUG
-#define DebugEnabled(expr) \
-    expr
+#define DebugEnabled(expr)  \
+                            \
+    expr                    \
+    
 #else 
-#define DebugEnabled(expr)
+#define DebugEnabled(expr)  \
+    /*                      \
+    expr                    \
+    */
 #endif /* DEBUG */
